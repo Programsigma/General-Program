49576c2afcac2473006fb000a271461a1f990cb9
Teed <26TeedJ@badinhs.org>
2025-02-12 13:22:48 -0500

    OrganizedBetter

---
+++ everything/Notes/loopsNotes.js
@@ -0,0 +1,21 @@
+/* loops are a great way for us to repeat a set of steps.
+If you have something you want to do multiple times in a row,
+ or until something happens, loops are an ideal solution
+
+ Two types of loops
+ -While loops
+ -For loops
+
+ While loops:
+ while(condition) {
+    // code goes here
+ }
+
+ code will repeat as long as the condition is true.
+
+ ex:  */
+ let i = 0;
+ while (i<3){
+    console.log(i);
+    i++; // shorthand for i += 1, a shorthand for i=i+1
+ }

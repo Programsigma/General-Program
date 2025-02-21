afc28997d40921a336b5c4b5fb43cb1d56560437
Teed <26TeedJ@badinhs.org>
2025-02-21 13:44:25 -0500

    loops practice v1

--- Practice/loopsPractice.js
+++ Practice/loopsPractice.js
@@ -1,1 +1,32 @@
-DOMPointReadOnly
+// for(let n = 0; n>=1, n<=21; n++) {
+//     if(n % 2 == 0){
+//         continue
+//     }
+//     console.log(n)
+// }
+
+// // 2. rewrite the code below with a while loop:
+
+// for (let i = 0; i < 3; i++) { 
+//     alert( `number ${i}!` ); 
+// }
+
+// let i = 0; 
+// while ( i < 3) { 
+//         alert( `number ${i}!` ); 
+// }
+
+// 3. Write a loop which prompts for a number greater than 100. If the visitor enters another number – ask them to input again.
+// The loop must ask for a number until either the visitor enters a number greater than 100 or cancels the input/enters an empty line.
+// Here we can assume that the visitor only inputs numbers. There’s no need to implement a special handling for a non-numeric input in this task.
+
+for(let n = prompt; n>100; n++) {
+    if(n<100) {
+        console.log(`${n} is not a valid number, please enter a number greater than 100`)
+    continue
+    }
+    elseif(n>100) {
+        console.log("You put a valid number in, Yayyyyy!")
+    }
+}
+

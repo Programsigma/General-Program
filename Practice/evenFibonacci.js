5270467ab702eee9cb3cb5dfd1c32c08bf7acceb
Teed <26TeedJ@badinhs.org>
2025-04-22 14:26:47 -0400

    Square Diff finished

---
+++ Practice/evenFibonacci.js
@@ -0,0 +1,7 @@
+
+function fibonacci(num){
+    for(sum = 0;sum<=4000000;i++ ) {
+    sum = sum + (i - 1)
+    }
+}
+console.log(fibonacci(0))
--- Practice/generalPractice.js
+++ Practice/generalPractice.js
@@ -1,12 +1,25 @@
-// Make a paper fortune teller in here
 
-let answer = undefined
-let letterNumber = 0
-function FortuneTeller(){
-    answer == prompt("Gimme a word")
-    answer.length = letterNumber
-if (letterNumber%2 == 0) {
-    
+let Matrix = [11,2,4,4,5,6,10,8,-12]
+let matrixSize = Math.sqrt(Matrix.length)
+let negativeSumvalue = 0
+let positiveSumvalue = 0
+function positiveSum(ar){
+    while(ar[i]!== undefined){
+        positiveSumvalue = positiveSumvalue + ar[(Math.sqrt(ar.length)-1) + ((Math.sqrt(ar.length)-1)*i)]
+        i++
+    }
+    return positiveSumvalue
+}
+function negativeSum(ar){
+    while(ar[i] !== undefined){
+     negativeSumValue = negativeSumvalue + ar[4*i]
+     i++
+   }
+   return negativeSumvalue
+}
+function squareDiff (ar){
+   squareDifference = negativeSum(ar)-positiveSum(ar)
+   return squareDifference
 }
 
-}
+console.log (squareDiff(Matrix))
